1ff26d076cf23cbf047f9642e154f499f1086864
Seamus <seamus.macgrianna12@gmail.com>
2021-01-10 16:04:33 +0000

    Add reload page function

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -10,7 +10,7 @@ var loggedIn = false;
 // Check username and store in localStorage if correct
 function checkName() {
   if (userName.value.length == 0) {
-    alert("Please enter a username!");
+    document.getElementById('errors').innerHTML = "Please enter a username!";
     return false;
   } else if (userName.value.length > 0) {
     localStorage.setItem('name', userName.value);
@@ -22,12 +22,12 @@ function checkName() {
 // Check password and store in localStorage if correct
 function checkPass() {
   if (userPw.value.length == 0) {
-    alert("Please enter a password!");
+    document.getElementById('errors').innerHTML = "Please enter a password!";
     return false;
   } else if (userPw.value.length < 8) {
-    alert("Please enter a password with 8 characters or more!");
+    document.getElementById('errors').innerHTML = "Please enter a password with 8 characters or more!";
     return false;
-  } else if (userPw.value == 'password') {
+  } else if (userPw.value == 'password' || userPw.value.length >= 8) {
     localStorage.setItem('password', userPw.value);
     localPassword = localStorage.getItem('password');
     return true;
@@ -37,36 +37,34 @@ function checkPass() {
 // Validate inputs, add loader and hide login form elements 
 $(document).ready(function () {
   $('#login').click(function () {
-    if (checkName() && checkPass()) {
+    if (checkName() && checkPass() && localName.length > 0 && localPassword == 'password') {
       $(this).prop("disabled", true);
       $(this).html(`<span class="spinner-border spinner-border-lg" role="status" aria-hidden="true"></span>`);
 
 
       setTimeout(function () {
-        if (localName.length > 0 && localPassword == 'password') {
-          loggedIn = true;
-          $(loginForm).children().hide();
-          $(loginForm).html("" +
-            `<i class="fas fa-check-circle"></i>` +
-            `<h1>Great, Login Success!</h1>` +
-            `<h2>${localName}, Welcome to Quiz-It!</h2>` +
-            `<hr>` +
-            `<button type="button" class="btn btn-primary btn-block btn-lg" id="start" onsubmit="login()">Start Quiz</button>`);
-        } 
+        loggedIn = true;
+        $(loginForm).children().hide();
+        $(loginForm).html("" +
+          `<i class="fas fa-check-circle"></i>` +
+          `<h1>Login Success!</h1>` +
+          `<h2>Welcome ${localName}!</h2>` +
+          `<hr>` +
+          `<button type="button" class="btn btn-primary btn-block btn-lg" id="start" onsubmit="login()">Start Quiz</button>`);
       }, 2000);
-    } else if (localPassword !== 'password') {
+    } else if (checkName() && checkPass() && localName.length > 0 && localPassword !== 'password') {
       $(this).prop("disabled", true);
       $(this).html(`<span class="spinner-border spinner-border-lg" role="status" aria-hidden="true"></span>`);
+      loggedIn = false;
 
       setTimeout(function () {
-          loggedIn = true;
-          $(loginForm).children().hide();
-          $(loginForm).html("" +
-            `<i class="fas fa-exclamation-circle"></i>` +
-            `<h1>Sorry, Wrong Password!</h1>` +
-            `<h2>${localName}, Please try again!</h2>` + 
-            `<hr>` +
-            `<button type="button" class="btn btn-primary btn-block btn-lg" id="retry" onsubmit="login()">Try Again</button>`);
+        $(loginForm).children().hide();
+        $(loginForm).html("" +
+          `<i class="fas fa-exclamation-circle"></i>` +
+          `<h1>Wrong Password!</h1>` +
+          `<h2>Please try again!</h2>` +
+          `<hr>` +
+          `<button type="button" class="btn btn-primary btn-block btn-lg" id="retry" onclick="reloadPage()">Try Again</button>`);
       }, 2000);
 
 
@@ -74,6 +72,11 @@ $(document).ready(function () {
 
   });
 });
+
+function reloadPage() {
+  window.location.reload();
+  return false;
+}
 
 // Change border of form name input when clicked
 $(document).ready(function () {
