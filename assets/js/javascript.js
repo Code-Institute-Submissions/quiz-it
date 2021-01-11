5f58f7f2bf665c84d86119c7987afcb8d5a3d882
Seamus <seamus.macgrianna12@gmail.com>
2021-01-11 23:43:27 +0000

    Add api call function

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,9 +3,42 @@ var userName = document.getElementById('username');
 var userPw = document.getElementById('password');
 var loginBtn = document.getElementById('login');
 var loginForm = document.getElementById("login-form");
+var quizContent = document.getElementsByClassName("form-check");
 var localName;
 var localPassword;
 var loggedIn = false;
+var data;
+var question;
+
+
+// Run API request on window load
+window.onload = sendApiRequest();
+
+
+//Get questions from the API
+async function sendApiRequest() {
+  const url = 'https://opentdb.com/api.php?amount=10&type=multiple';
+  const response = await fetch(url);
+  console.log(response);
+  data = await response.json();
+  console.log(data);
+
+  if (!response.ok) {
+    document.getElementById('errors').innerHTML = `Sorry a ${response.status} error has occured. Try again later.`;
+  } else {
+    fillForm();
+  }
+}
+
+//Fill form with question and answers
+function fillForm() {
+  // document.getElementById('question').innerHTML = `${data.results[0].question}`;
+  document.getElementById('question').innerHTML = '';
+  for (i = 0; i < data.results.length; i++) {
+    window['question' + i] = document.getElementById('question').innerHTML = `${data.results[i].question}`;
+  }
+  console.log(question1, question2);
+}
 
 // Check username and store in localStorage if correct
 function checkName() {
@@ -45,6 +78,7 @@ $(document).ready(function () {
       setTimeout(function () {
         loggedIn = true;
         $(loginForm).children().hide();
+        $('.main-container').append(`<button type="button" class="btn btn-danger logout-btn" onclick="reloadPage()">Logout</button>`);
         $(loginForm).html("" +
           `<i class="fas fa-check-circle"></i>` +
           `<h1>Login Success!</h1>` +
