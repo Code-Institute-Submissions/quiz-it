cfd1c2b9eb4ecab101b8481bf9260943afc38494
Seamus <seamus.macgrianna12@gmail.com>
2021-01-14 23:05:43 +0000

    Modify fillForm function

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,7 +3,7 @@ var userName = document.getElementById('username');
 var userPw = document.getElementById('password');
 var loginBtn = document.getElementById('login');
 var loginForm = document.getElementById("login-form");
-var quizContent = document.getElementsByClassName("form-check");
+var quizForm = document.getElementById("quiz");
 var localName;
 var localPassword;
 var loggedIn = false;
@@ -12,8 +12,9 @@ var question;
 
 
 // Run API request on window load
-window.onload = sendApiRequest();
-
+function startQuiz() {
+  sendApiRequest();
+}
 
 //Get questions from the API
 async function sendApiRequest() {
@@ -26,19 +27,82 @@ async function sendApiRequest() {
   if (!response.ok) {
     document.getElementById('errors').innerHTML = `Sorry a ${response.status} error has occured. Try again later.`;
   } else {
-    fillForm();
+    fillForm(data.results);
   }
 }
 
 //Fill form with question and answers
-function fillForm() {
-  // document.getElementById('question').innerHTML = `${data.results[0].question}`;
-  document.getElementById('question').innerHTML = '';
-  for (i = 0; i < data.results.length; i++) {
-    window['question' + i] = document.getElementById('question').innerHTML = `${data.results[i].question}`;
+function fillForm(data) {
+  console.log("Success!");
+  $(loginForm).children().remove();
+  $(quizForm).children().remove();
+  for (index = 0; index < data.length; index++) {
+    const element = data[index];
+    const question = data[index].question;
+    var answer = getAnswers(element);
+    shuffle(answer);
+    console.log(question);
+    console.log(answer);
+    console.log("question-" + (index + 1));
+    $(quizForm).append("" +
+      `<div class="question${index + 1}" style="display: none;">` +
+      `<h2>The Quiz:</h2>` +
+      `<div class="progress-bar">` +
+      `</div>` +
+      `<hr>` +
+      `<p id="question">Question ${index + 1}:${question}</p>` +
+      `<hr>` +
+      `<div class="custom-control custom-radio text-left p-3 quiz-content">` +
+      `<div class="form-check">` +
+      `<input type="radio" id="answer-1" name="answer" class="custom-control-input">` +
+      `<label for="answer-1" class="custom-control-label">${answer[0]}</label>` +
+      `</div>` +
+      `<div class="form-check">` +
+      `<input type="radio" id="answer-2" name="answer" class="custom-control-input">` +
+      `<label for="answer-2" class="custom-control-label">${answer[1]}</label>` +
+      `</div>` +
+      `<div class="form-check">` +
+      `<input type="radio" id="answer-3" name="answer" class="custom-control-input">` +
+      `<label for="answer-3" class="custom-control-label">${answer[2]}</label>` +
+      `</div>` +
+      `<div class="form-check">` +
+      `<input type="radio" id="answer-4" name="answer" class="custom-control-input">` +
+      `<label for="answer-4" class="custom-control-label">${answer[3]}</label>` +
+      `</div>` +
+      `</div>` +
+      `<button type="button" class="btn btn-primary btn-block btn-lg">Next</button>` +
+      `</div>`
+    );
   }
-  console.log(question1, question2);
+  document.querySelector('.question1').style.display = 'block';
 }
+
+// Fisher-Yates (aka Knuth) Shuffle algorithm - function borrowed in full from: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
+  console.log(currentIndex)
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+function getAnswers(answers) {
+  // totalAnswers.push(answers.correct_answer);
+  // totalAnswers.push(answers.incorrect_answers);
+  var totalAnswers = answers.incorrect_answers.concat(answers.correct_answer);
+  return totalAnswers;
+}
+
 
 // Check username and store in localStorage if correct
 function checkName() {
@@ -84,7 +148,7 @@ $(document).ready(function () {
           `<h1>Login Success!</h1>` +
           `<h2>Welcome ${localName}!</h2>` +
           `<hr>` +
-          `<button type="button" class="btn btn-primary btn-block btn-lg" id="start" onsubmit="login()">Start Quiz</button>`);
+          `<button type="button" class="btn btn-primary btn-block btn-lg" id="start" onclick="startQuiz()">Start Quiz</button>`);
       }, 2000);
     } else if (checkName() && checkPass() && localName.length > 0 && localPassword !== 'password') {
       $(this).prop("disabled", true);
